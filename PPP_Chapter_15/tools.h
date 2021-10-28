d1a6af40582ca4fb1eab24dd7e402eec5c176585
stelatoris <78478872+stelatoris@users.noreply.github.com>
2021-10-28 14:57:50 +0200

    Add project files.

---
+++ PPP_Chapter_15/tools.h
@@ -0,0 +1,13 @@
+// tools.h
+
+#include "Graph.h"
+#include "std_lib_facilities.h"
+#define PI 3.14159265
+
+using namespace Graph_lib;
+
+namespace Graph_lib {
+
+
+}	// end of namespace Graph_lib
+
