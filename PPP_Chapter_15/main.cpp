eefa818ff331ca3d513fcd39c7fd15cca9b0074e
stelatoris <78478872+stelatoris@users.noreply.github.com>
2021-10-30 23:15:06 +0200

    Drill Class complete.

--- PPP_Chapter_15/main.cpp
+++ PPP_Chapter_15/main.cpp
@@ -11,39 +11,47 @@
 
 using namespace Graph_lib;
 
-
-struct Name {
-    
-};
-
 struct Person {
-    Person(std::string name, int age);
-    Person() : n{ "" }, a{ 0 } {}
-    std::string name() const { return n; }
+    Person(std::string f_name, std::string s_name, int age);
+    Person() : f_n{ "" }, s_n{ "" }, a{ 0 } {}
+
+    std::string first_name() const { return f_n; }
+    std::string second_name() const { return s_n; }
     int age() const { return a; }
 
-    void set_name(const std::string& name) { n = name; }
+    void set_name(const std::string f_name, std::string s_name) 
+    {
+        f_n = f_name; s_n = s_name;
+    }
     void set_age(int age) { a = age; }
 private:
-    std::string n;
+    std::string f_n;
+    std::string s_n;
     int a;
 };
 
-Person::Person(std::string name, int age) 
-    : n{ name }, a{ age } 
+Person::Person(std::string f_name, std::string s_name, int age)
+    : f_n{ f_name }, s_n{ s_name }, a{ age }
 {
     const std::string false_chars{ ";:\"'[]*&^%$#@!" };
-    for (char c1 : name) {        
+    for (char c1 : f_name) {    // first name
         for (char c2 : false_chars) {
             if (c1 == c2) error("name has an invalid character");
         }
     }
+    for (char c1 : s_name) {    // second name
+        for (char c2 : false_chars) {
+            if (c1 == c2) error("name has an invalid character");
+        }
+    }
+
     if (0 > age || age > 150) error("age must be in 0:150 range");
 }
 
 bool operator==(const Person& p1, const Person& p2)
 {
-    return p1.name() == p2.name()
+    return p1.first_name() == p2.first_name()
+        && p1.second_name() == p2.second_name()
         && p1.age() == p2.age();
 }
 
@@ -52,10 +60,10 @@ bool operator!=(const Person& p1, const Person& p2)
     return !(p1 == p2);
 }
 
-
 ostream& operator<<(ostream& os, Person& const p)
 {
-    os <<'('<< p.name() << ", " << p.age() <<')';
+    os <<'('<< p.first_name() <<' ' << p.second_name()
+        << ", " << p.age() <<')';
     return os;
 }
 
@@ -63,15 +71,15 @@ istream& operator>>(istream& is, Person& p)
 // read name and age
 // format: name,age
 {
-    std::string n;
+    std::string f_n;
+    std::string s_n;
     int a;
 
-    is >> n >> a;
+    is >> f_n >> s_n >> a;
     if (!is) return is;
-    p = Person{ n,a };
+    p = Person{ f_n,s_n,a };
     return is;
 }
-
 
 int main()
 try
@@ -89,7 +97,8 @@ try
         
         bool repeat = false;        // repeated in put
         for (int i = 0; i < v_persons.size(); ++i) {    // look for double input
-            if (p1 == v_persons[i]) {                
+            if (p1.first_name() == v_persons[i].first_name() &&
+                p1.second_name() == v_persons[i].second_name()) {
                 repeat = true;
                 break;
             }                
